be101f8a0d6834a0e1342108c7f4c01993f1166e
Gagan Deep <157812500+gagan889@users.noreply.github.com>
2025-03-14 17:36:26 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,7 +1,10 @@
-def call(){
-           sh  ''' ${scannerHome}/bin/sonar-scanner \
-                  -Dsonar.projectKey=sonar-demo-org-april_gagan44 \
-                  -Dsonar.java.binaries=target/classes \
-                  -Dsonar.organization=sonar-demo-org-april
-              ''' }
+def call() {
+    sh '''
+        ${scannerHome}/bin/sonar-scanner \
+        -Dsonar.projectKey=sonar-demo-org-april_gagan44 \
+        -Dsonar.java.binaries=target/classes \
+        -Dsonar.organization=sonar-demo-org-april
+    '''
+}
+
   
