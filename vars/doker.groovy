568bd48f75e7567561ffc46911f52155183dc13f
Gagan Deep <157812500+gagan889@users.noreply.github.com>
2025-04-11 15:26:29 +0530

    Update doker.groovy

--- vars/doker.groovy
+++ vars/doker.groovy
@@ -1,9 +1,9 @@
 docker call(){
   echo "Start pushing the image to Docker-Hub"
-                withCredentials([usernamePassword(credentialsId: 'jenkins-cred', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-                    sh """
-                        docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
-                        docker image tag notes-app:latest ${DOCKER_USER}/notes-app:latest
-                        docker push ${DOCKER_USER}/notes-app:latest
-                    """
-}
+  withCredentials([usernamePassword(credentialsId: 'jenkins-cred', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+  sh """
+    docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
+    docker image tag notes-app:latest ${DOCKER_USER}/notes-app:latest
+    docker push ${DOCKER_USER}/notes-app:latest
+                    """ 
+  }
