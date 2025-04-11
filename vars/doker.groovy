63f63a50ea0be38d3578d76bbb820d5ae3ae121d
Gagan Deep <157812500+gagan889@users.noreply.github.com>
2025-04-11 15:53:07 +0530

    Update doker.groovy

--- vars/doker.groovy
+++ vars/doker.groovy
@@ -1,9 +1,7 @@
 def call() {
     withCredentials([usernamePassword(credentialsId: 'jenkins-cred', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-    ssh """
-            docker login -u ${env.DOCKER_USER} -p ${env.DOCKER_PASS}           
-            docker image tag notes-app:latest ${env.DOCKER_USER}/notes-app:latest
-            docker push ${env.DOCKER_USER}/notes-app:latest
-        """
+    sh 'docker login -u ${env.DOCKER_USER} -p ${env.DOCKER_PASS}'           
+    sh 'docker image tag notes-app:latest ${env.DOCKER_USER}/notes-app:latest'
+    sh  'docker push ${env.DOCKER_USER}/notes-app:latest'
     }
 }
